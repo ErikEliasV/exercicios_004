6821e575d8b99f5c936a29981c2e1f29312fe9c1
ERIK ELIAS VARELA DE SOUSA <a95415195@rn.senac.local>
2025-07-07 14:39:12 -0300

    feat: questão 02 - implementação de input e botão para adicionar tarefa

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Title } from './styles'
+import { Container, Title, Input, ButtonAdd, ButtonAddText, ViewRow } from './styles'
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <Container>
             <Title>Minhas Tarefas.</Title>
+            <ViewRow>
+                <Input
+                    placeholder='Adicione uma nova tarefa'
+                    placeholderTextColor='#888888'
+                />
+                <ButtonAdd>
+                    <ButtonAddText>+</ButtonAddText>
+                </ButtonAdd>
+            </ViewRow>
         </Container>
     )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -13,3 +13,37 @@ export const Title = styled.Text`
     font-size: 24px;
     font-weight: bold;
 `
+
+export const Input = styled.TextInput`
+    background-color: #1E1E1E;
+    border-radius: 5px;
+    width: 80%;
+    padding: 10px;
+    color: #FFFFFF;
+    border-width: 1px;
+    border-color: #333333;
+`
+
+export const ButtonAdd = styled.TouchableOpacity`
+    background-color: #1DB954;
+    border-radius: 5px;
+    align-items: center;
+    justify-content: center;
+    border-color: #2A2A2A;
+    width: 40px;
+    height: 40px;
+`
+export const ButtonAddText = styled.Text`
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: bold;
+`
+export const ViewRow = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    margin-top: 30px;
+`
+
