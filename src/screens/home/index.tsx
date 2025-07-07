b3bea637cdccc32cc35fc32a600d9f93311f7e50
ERIK ELIAS VARELA DE SOUSA <a95415195@rn.senac.local>
2025-07-07 14:13:40 -0300

    feat: questão 01 - implementação inicial das telas

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,9 @@
-import { Container } from './styles'
+import { Container, Title } from './styles'
 
 export function Home(){
     return(
         <Container>
-
+            <Title>Minhas Tarefas.</Title>
         </Container>
     )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,8 +1,15 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import { SafeAreaView } from 'react-native';
 
 export const Container = styled(SafeAreaView)`
     background-color: #121212;
     align-items: center; 
     justify-content: center;
+    flex: 1;
+`
+
+export const Title = styled.Text`
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: bold;
 `
